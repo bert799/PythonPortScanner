ff2a8853fad941771e6ed550ce2bb4c780787a74
Bernardo <bernardocc@al.insper.edu.br>
2023-03-18 16:27:24 -0300

    scan de rede pronto

--- main.py
+++ main.py
@@ -22,43 +22,55 @@ def main_menu():
     3. Exit{bcolors.ENDC}
     ''')
 
+def clear_terminal():
+    print('\033c')
+
 def select_network_ip():
     ip = input('Enter the IP of a host or a network to scan: ')
     return ip
 
-# detect if input is a ip with a mask or a single ip
 def detect_ip_type(ip):
     if '/' in ip:
         return 'network'
     else:
         return 'host'
 
-def scan_network():
+def get_hostname(ip):
+    try:
+        return socket.gethostbyaddr(ip)
+    except socket.herror:
+        return 'Unknown'
+
+def scan_ip(ip):
+    request = scapy.ARP()
+    request.pdst = ip
+    broadcast = scapy.Ether()
+    broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+    request_broadcast = broadcast / request
+    answered_list = scapy.srp(request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list
+
+def scan_ip_menu():
     ip = select_network_ip()
     ip_type = detect_ip_type(ip)
-    request = scapy.ARP()
     if ip_type == 'network':
         print('Scanning network')
-        request = scapy.ARP()
-        request.pdst = ip
-        broadcast = scapy.Ether()
-        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-        request_broadcast = broadcast / request
-        answered_list = scapy.srp(request_broadcast, timeout=1, verbose=False)[0]
-        print(f'{bcolors.OKGREEN}Hosts up:{bcolors.ENDC}')
+        answered_list = scan_ip(ip)
+        print(f'{bcolors.OKGREEN}Hosts up:')
         for element in answered_list:
-            print(element[1].psrc)
-        print(f'{bcolors.FAIL}Hosts down:{bcolors.ENDC}')
+            print(element[1].psrc, ' - ', get_hostname(element[1].psrc)[0], ' - ', element[1].hwsrc)
+        print(f'{bcolors.ENDC}')
     elif ip_type == 'host':
         print('Scanning host')
-        if s.connect_ex((ip, 80)) == 0:
-            print(f'{bcolors.OKGREEN}Host up: {ip}{bcolors.ENDC}')
+        answered_list = scan_ip(ip)
+        if len(answered_list) != 0:
+            print(f'{bcolors.OKGREEN}Host {ip} is up{bcolors.ENDC}')
         else:
-            print(f'{bcolors.FAIL}Host down: {ip}{bcolors.ENDC}')
+            print(f'{bcolors.FAIL}Host {ip} is down{bcolors.ENDC}')
     else:
         print(f'{bcolors.FAIL}Invalid IP or network{bcolors.ENDC}')
         
-def scan_ports():
+def scan_ports_menu():
     print('Scanning ports')
     for i in range(1, 1024):
         if s.connect_ex((' ', i)) == 0:
@@ -66,16 +78,18 @@ def scan_ports():
 
 def main():
     while True:
+        clear_terminal()
         main_menu()
         choice = input('Enter your choice: ')
         if choice == '1':
-            scan_network()
+            scan_ip_menu()
         elif choice == '2':
-            scan_ports()
+            scan_ports_menu()
         elif choice == '3':
             break
         else:
             print('Invalid choice')
+        input('Press enter to continue')
 
 if __name__ == '__main__':
     main()
